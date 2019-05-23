68eb96dd898919c92f4977692c762d0ddd0a5393
fadilnatakusumah <fadil.ntksmh@gmail.com>
2019-05-23 20:43:38 +0700

    finish adding starting component before installing react react-dom react-router-dom

---
+++ src/containers/Users.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react'
+
+import PizzaImage from "../components/PizzaImage/PizzaImage";
+
+class Users extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Awesome Users</h1>
+        <p>Dummy text haha you're stupid</p>
+      </div>
+    )
+  }
+}
+
+export default Users
